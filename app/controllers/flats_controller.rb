1bb2106f10e2bb12c540dea0dc15eec0bc93ab85
Nadeesha Illamperuma <nadisliit@gmail.com>
2025-03-13 17:05:02 +0100

    create flats functionality

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -10,20 +10,26 @@ class FlatsController < ApplicationController
 
   def new
     @flat = Flat.new
+    render :add
+  end
+  def add
+    @flat = Flat.new
+    render :add
   end
 
   def create
     @flat = Flat.new(flat_params)
     @flat.user = current_user
     if @flat.save
-      redirect_to @flat, notice: 'Flat was successfully created.'
+      redirect_to flats_path(@flat), notice: "Flat successfully created!"
     else
-      render :new
+      render :add, status: unprocessable_entity
     end
   end
 
+
   private
-  
+
   def flat_params
     params.require(:flat).permit(:title, :description, :price, :address)
   end
